de7b24a32abd00aed746a66390b6f41fde24f242
marco.baratto <marcobaratto@icloud.com>
2022-01-16 15:47:46 +0100

    init radio buttons updated download button's click listener to handle selected radio button and assign relative value

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -9,6 +9,7 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -23,27 +24,38 @@ class MainActivity : AppCompatActivity() {
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
-        registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
-
-        custom_button.setOnClickListener {
-            download()
-        }
-    }
-
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
         }
     }
 
-    private fun download() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+        downloadButtonListener()
+    }
+
+    private fun downloadButtonListener () {
+        custom_button.setOnClickListener {
+            val selectedRadio  = findViewById<RadioButton>(radio_group.checkedRadioButtonId)
+            val url = when(selectedRadio.text){
+                getString(R.string.glide_repo) -> "${PREFIX_URL}/bumptech/glide${SUFFIX_URL}"
+                getString(R.string.project_repo) -> "${PREFIX_URL}/udacity/nd940-c3-advanced-android-programming-project-starter${SUFFIX_URL}"
+                getString(R.string.retrofit_repo) -> "${PREFIX_URL}/square/retrofit${SUFFIX_URL}"
+                else -> return@setOnClickListener
+            }
+            val fullUrl = "${PREFIX_URL}${url}${SUFFIX_URL}"
+            download(fullUrl)
+        }
+    }
+
+    private fun download(url: String) {
         val request =
-            DownloadManager.Request(Uri.parse(URL))
+            DownloadManager.Request(Uri.parse(url))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
@@ -51,13 +63,12 @@ class MainActivity : AppCompatActivity() {
                 .setAllowedOverRoaming(true)
 
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-        downloadID =
-            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+        downloadID = downloadManager.enqueue(request)// enqueue puts the download request in the queue.
     }
 
     companion object {
-        private const val URL =
-            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val PREFIX_URL = "https://github.com"
+        private const val SUFFIX_URL = "/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
     }
 
